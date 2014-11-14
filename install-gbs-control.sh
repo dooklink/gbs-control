6e959ede6011d3979fa92cd7116d2a46e2b9fd8c
Dooklink <dooklink@gmail.com>
2014-11-14 13:35:05 +1000

    Install Testing
    Temp changes for testing install

--- install-gbs-control.sh
+++ install-gbs-control.sh
@@ -3,9 +3,10 @@
 
 # Update sources and install I2C components.
 apt-get update
-apt-get install i2c-tools libi2c-dev python-smbus
+apt-get install -y i2c-tools libi2c-dev python-smbus
 
 # Get latest stable version from GitHub
+cd ~/
 wget https://raw.githubusercontent.com/dooklink/gbs-control/latest/gbs-control.zip
 
 # Unpack scripts & default settings
@@ -13,13 +14,13 @@ unzip -o gbs-control-latest.zip
 
 # Patch /etc/inittab to allow for automatic login 
 # and to use xterm-mono for B&W (monochrome) interactive terminal.
-patch -b /etc/inittab scripts/inittab.patch
+patch -b /etc/inittab ~/scripts/inittab.patch
 
 # Move Triggerhappy conf files to /etc/triggerhappy/
-cp thd/* /etc/triggerhappy/triggers.d/*
+#cp thd/* /etc/triggerhappy/triggers.d/*
 
 # Add required scripts for automatic start-up.
-patch -b /etc/profile profile.patch
+#patch -b /etc/profile profile.patch
 
 # Reboot
 sync
